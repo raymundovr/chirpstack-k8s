1e487a0b1d2c0579badc61c2512fa48e463468df
Raymundo Vásquez Ruiz <raymundo@levv.io>
2024-10-18 11:48:05 +0200

    fix configmaps path after moving

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -2,6 +2,6 @@
 kubectl create ns chirpstack
 kubectl config set-context --current --namespace=chirpstack
 kubectl apply -f mosquitto.yaml -f redis.yaml -f postgres.yaml
-kubectl apply -f configmaps/chirpstack-config-1.yaml -f configmaps/chirpstack-config-2.yaml -f chirpstack-gw-configmap.yaml
+kubectl apply -f configmaps/chirpstack-config-1.yaml -f configmaps/chirpstack-config-2.yaml -f configmaps/chirpstack-gw-configmap.yaml
 kubectl apply -f api.yaml -f bridge.yaml -f app.yaml
 kubectl config set-context --current --namespace=default
--- kubernetes/uninstall.sh
+++ kubernetes/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kubectl config set-context --current --namespace=chirpstack
 kubectl delete -f mosquitto.yaml -f redis.yaml -f postgres.yaml
-kubectl delete -f configmaps/chirpstack-config-1.yaml -f configmaps/chirpstack-config-2.yaml -f chirpstack-gw-configmap.yaml
+kubectl delete -f configmaps/chirpstack-config-1.yaml -f configmaps/chirpstack-config-2.yaml -f configmaps/chirpstack-gw-configmap.yaml
 kubectl delete -f api.yaml -f bridge.yaml -f app.yaml
 kubectl delete ns chirpstack
 kubectl config set-context --current --namespace=default
